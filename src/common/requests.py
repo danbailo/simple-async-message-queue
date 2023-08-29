a459c33cc6932c06379bf03a4bc384db7c52eae8
Daniel Bailo <danbailoufms@gmail.com>
2023-08-28 21:13:33 -0300

    feat: async method to fetch and lock

--- src/common/requests.py
+++ src/common/requests.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from typing import Any
 
 from httpx import AsyncClient
@@ -6,7 +8,7 @@ from httpx import AsyncClient
 async def async_fetch(
     method: str,
     url: str,
-    body: dict[str, Any],
+    body: dict[str, Any] | None = None,
     cookies: dict[str, Any] | None = None,
     headers: dict[str, str] | None = None,
     raise_for_status: bool = False,
@@ -16,6 +18,8 @@ async def async_fetch(
         cookies = {}
     if not headers:
         headers = {'Content-Type': 'application/json'}
+    if not body:
+        body = {}
     async with AsyncClient() as client:
         response = await client.request(
             method, url, json=body, headers=headers, timeout=timeout
@@ -26,6 +30,14 @@ async def async_fetch(
 
 
 async def async_post_create_record(item: dict[str, Any]):
+    await asyncio.sleep(3)
+    url = 'http://localhost:8000/record'
     return await async_fetch(
-        'post', 'http://localhost:8000/record', body=item
+        'post', url, body=item
     )
+
+
+async def async_get_fetch_and_lock():
+    url = 'http://localhost:8000/record/fetch-and-lock'
+    if (result := await async_fetch('get', url)):
+        yield result
