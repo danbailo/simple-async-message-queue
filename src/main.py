6548f398acc64cfa7065b9a0c5a717b178518740
Daniel Bailo <danbailoufms@gmail.com>
2023-08-30 19:44:26 -0300

    refactor: timing runtime of insert records command

--- src/main.py
+++ src/main.py
@@ -2,12 +2,11 @@ import asyncio
 
 from functools import wraps
 
-import time
-
 from pandas import read_excel
 
 import typer
 
+from common.decorators import timeit
 from common.logger import logger
 from common.requests import async_post_create_record
 
@@ -44,14 +43,12 @@ async def async_consume_single(worker: WorkerEnum):
 
 
 @app.command()
-def insert_data_from_df(path: str = typer.Option()):
+@timeit
+def insert_records_from_excel(path: str = typer.Option()):
     df = read_excel(path)
-    start = time.monotonic()
-    for it, item in enumerate(df.to_dict(orient='records')):
+    for it, item in enumerate(df.to_dict(orient='records'), start=1):
         asyncio.run(async_post_create_record(item))
         logger.info(f'progress {it}/{len(df)}')
-    end = time.monotonic()
-    logger.info(f'time elapsed - {end - start:0.2f}s')
 
 
 if __name__ == '__main__':
